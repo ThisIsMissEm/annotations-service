a580320ba5c6eebe9f7cd21f169752fafb667be2
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2024-10-30 23:11:41 +0100

    Fix database setup for deployment

--- config/database.ts
+++ config/database.ts
@@ -1,22 +1,38 @@
+import type { ConnectionOptions } from 'node:tls'
+
+import fs from 'node:fs'
 import env from '#start/env'
+import app from '@adonisjs/core/services/app'
 import { defineConfig } from '@adonisjs/lucid'
+
+// Type compatible with the `ssl` option from Adonis:
+let sslOptions: boolean | ConnectionOptions | undefined = false
+
+if (env.get('DATABASE_USE_SSL')) {
+  sslOptions = {
+    ca: fs.readFileSync(app.makePath('database', 'ca.pem'), 'ascii'),
+  }
+}
 
 const dbConfig = defineConfig({
   connection: 'postgres',
+
   connections: {
     postgres: {
       client: 'pg',
       connection: {
-        host: env.get('DB_HOST'),
-        port: env.get('DB_PORT'),
-        user: env.get('DB_USER'),
-        password: env.get('DB_PASSWORD'),
-        database: env.get('DB_DATABASE'),
+        host: env.get('DATABASE_HOST'),
+        port: env.get('DATABASE_PORT'),
+        user: env.get('DATABASE_USER'),
+        password: env.get('DATABASE_PASSWORD', ''),
+        database: env.get('DATABASE_NAME'),
+        ssl: sslOptions,
       },
       migrations: {
         naturalSort: true,
         paths: ['database/migrations'],
       },
+      debug: false,
     },
   },
 })
--- start/env.ts
+++ start/env.ts
@@ -13,10 +13,12 @@ import { Env } from '@adonisjs/core/env'
 
 export default await Env.create(new URL('../', import.meta.url), {
   NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
-  PORT: Env.schema.number(),
-  APP_KEY: Env.schema.string(),
+  LOG_LEVEL: Env.schema.enum(['fatal', 'error', 'warn', 'info', 'debug', 'trace'] as const),
+
   HOST: Env.schema.string({ format: 'host' }),
-  LOG_LEVEL: Env.schema.string(),
+  PORT: Env.schema.number(),
+
+  APP_KEY: Env.schema.string(),
 
   /*
   |----------------------------------------------------------
@@ -30,9 +32,10 @@ export default await Env.create(new URL('../', import.meta.url), {
   | Variables for configuring database connection
   |----------------------------------------------------------
   */
-  DB_HOST: Env.schema.string({ format: 'host' }),
-  DB_PORT: Env.schema.number(),
-  DB_USER: Env.schema.string(),
-  DB_PASSWORD: Env.schema.string.optional(),
-  DB_DATABASE: Env.schema.string()
+  DATABASE_HOST: Env.schema.string({ format: 'host' }),
+  DATABASE_PORT: Env.schema.number(),
+  DATABASE_USER: Env.schema.string(),
+  DATABASE_PASSWORD: Env.schema.string.optional(),
+  DATABASE_NAME: Env.schema.string(),
+  DATABASE_USE_SSL: Env.schema.boolean.optional(),
 })
