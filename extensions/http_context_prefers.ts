1859d26c4c0ad9fd00536911265b304605b528db
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2024-11-02 01:08:20 +0100

    Change prefersRepresentation to be more specialized to Web Annotations protocol

--- extensions/http_context_prefers.ts
+++ extensions/http_context_prefers.ts
@@ -1,14 +1,29 @@
+import {
+  PREFER_CONTAINED_DESCRIPTION,
+  PREFER_CONTAINED_IRIS,
+  PREFER_MINIMAL_CONTAINER,
+} from '#utils/constants'
 import { Request } from '@adonisjs/core/http'
 import parsePreferHeader from 'parse-prefer-header'
 
 declare module '@adonisjs/core/http' {
   interface Request {
-    prefersRepresentation(): string[] | false
+    prefersRepresentation(): {
+      iris: boolean
+      descriptions: boolean
+      minimal: boolean
+    }
   }
 }
 
 Request.macro('prefersRepresentation', function (this: Request) {
-  if (!this.header('prefer')) return []
+  if (!this.header('prefer')) {
+    return {
+      minimal: true,
+      iris: false,
+      descriptions: false,
+    }
+  }
 
   const prefers = parsePreferHeader(this.header('prefer'))
 
@@ -16,8 +31,23 @@ Request.macro('prefersRepresentation', function (this: Request) {
     !prefers.return ||
     prefers.return !== 'representation' ||
     typeof prefers.returnInclude !== 'string'
-  )
-    return false
+  ) {
+    return {
+      minimal: true,
+      iris: false,
+      descriptions: false,
+    }
+  }
 
-  return prefers.returnInclude.split(/\s+/)
+  const returnInclude = prefers.returnInclude.split(/\s+/)
+
+  const prefersMinimal = returnInclude.includes(PREFER_MINIMAL_CONTAINER)
+  const prefersIris = returnInclude.includes(PREFER_CONTAINED_IRIS)
+  const prefersDescriptions = returnInclude.includes(PREFER_CONTAINED_DESCRIPTION)
+
+  return {
+    minimal: prefersMinimal,
+    iris: prefersIris,
+    descriptions: prefersDescriptions,
+  }
 })
